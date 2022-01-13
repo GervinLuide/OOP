958a7c0fa2c5ee3d630ff65f599b771528f8d001
Gervin Luide <gervin.luide@khk.ee>
2022-01-13 09:40:01 +0200

    Ül4 restoraan

--- praks2/restoraan.py
+++ praks2/restoraan.py
@@ -7,3 +7,4 @@ class Restoraan():
 
     def ava_restoraan(self):
         print("restoraan on avatud")
+
--- praks2/restoraanid.py
+++ praks2/restoraanid.py
@@ -2,7 +2,23 @@ from praks2.restoraan import Restoraan
 
 minu_restoraan = Restoraan()
 minu_restoraan.restoraani_nimi = "Gervini resto"
-minu_restoraan.soogi_tyyp = "maisev toit"
+minu_restoraan.soogi_tyyp = "maitsvat toitu"
 
 minu_restoraan.kirjelda_restoraan()
 minu_restoraan.ava_restoraan()
+
+
+sinu_restoraan = Restoraan()
+sinu_restoraan.restoraani_nimi = "Danili resto"
+sinu_restoraan.soogi_tyyp = "Maitsvamat toitu"
+
+sinu_restoraan.kirjelda_restoraan()
+sinu_restoraan.ava_restoraan()
+
+
+sobra_restoraan = Restoraan()
+sobra_restoraan.restoraani_nimi = "Viktori resto"
+sobra_restoraan.soogi_tyyp = "vägevalt maitsvat toitu"
+
+sobra_restoraan.kirjelda_restoraan()
+sobra_restoraan.ava_restoraan()
