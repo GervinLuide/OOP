774aa548f825acab38d0b9c139aadbd1f54c7bd3
Gervin Luide <gervin.luide@khk.ee>
2022-01-20 08:51:44 +0200

    Ül6 restoraan2

--- praks2/restoraanid2.py
+++ praks2/restoraanid2.py
@@ -3,3 +3,11 @@ from praks2.restoraan2 import Restoraan
 esimene = Restoraan("minu resto", "pizza")
 
 esimene.kirjelda_restoraan()
+
+teine = Restoraan("Danil", "maitselisi kooke")
+
+teine.kirjelda_restoraan()
+
+kolmas = Restoraan("Viktor", "banaani")
+
+kolmas.kirjelda_restoraan()
